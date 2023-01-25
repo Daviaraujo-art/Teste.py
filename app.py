1774acd0b464a0bdb92bccd6f10977988f218bb7
Davi Araujo <110649086+Daviaraujo-art@users.noreply.github.com>
2023-01-24 21:32:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,32 +4,50 @@ import constants
 from my_structure import NumberStorage
 
 
-def get_filename_arg():
+def get_filenames_arg():
     args = sys.argv
     if len(args) < constants.MIN_ARGS:
         raise Exception("You must pass at least one argument")
     if args[1].endswith(".txt") is False:
         raise Exception("The file argument must end with .txt")
     # TODO: validate if the file, actually exists.
-    return args[1]
+    return args[1:]
 
 
 def run():
-    file_name = get_filename_arg()
-    storage = NumberStorage()
-    with open(file_name, "r") as input_file:
-        for line in input_file:
-            try:
-                number = int(line)
-            except Exception as e:
-                print(e)
-                # raise Exception("...")
-                continue
-            storage.add_number(number)
+    file_names = get_filenames_arg()
+    for file_name in file_names:
+        storage = NumberStorage()
+        with open(file_name, "r") as input_file:
+            for line in input_file:
+                try:
+                    number = int(line)
+                except Exception as e:
+                    print(e)
+                    # raise Exception("...")
+                    continue
+                storage.add_number(number)
 
-    absolute_path = os.path.dirname(__file__)
-    file_path = absolute_path+"/"+constants.OUTPUT_FILE_NAME
-    with open(file_path, "w") as output_file:
-        for number, status in storage.get_numbers().items():
-            output_file.write(status + ": " + str(number) + "\n")
-            print(number, status)
+        absolute_path = os.path.dirname(__file__)
+        file_path = absolute_path + "/"
+        first_part_of_file = file_name.split(".")[0]
+        file_path += first_part_of_file + "_" + constants.OUTPUT_FILE_NAME
+        with open(file_path, "w", 0o777) as output_file:
+            print("output for: ", file_name)
+            for number, status in storage.get_numbers().items():
+                output_file.write(status + ": " + str(number) + "\n")
+                print(number, status)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
